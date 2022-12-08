bc9316462bdb1303a3ddf6a8db96a588a66055e7
Aivech <16143392+Aivech@users.noreply.github.com>
2022-12-08 10:27:23 -0500

    Day 1 part 2

--- src/bin/day-1.rs
+++ src/bin/day-1.rs
@@ -20,17 +20,31 @@ fn main() {
         Err(msg) => {panic!("Couldn't open \"{}\": {}", path.display(), msg)}
     };
 
-    let res = lines.fold((0,0), |(accum, max), line| -> (i32, i32){
+    let res = lines.fold((0,0,0,0), |(accum, max1, max2, max3), line| -> (i32, i32, i32, i32){
         match line {
             Ok(str) => if str.is_empty() {
-                if accum > max { (0, accum) } else { (0, max) }
+                if accum > max3 {
+                    if accum > max2 {
+                        if accum > max1 {
+                            return (0, accum, max1, max2)
+                        }
+                        return (0, max1, accum, max2)
+                    }
+                    (0, max1, max2, accum)
+                } else { (0, max1, max2, max3) }
             } else { match str.parse::<i32>() {
-                Ok(int) => (accum + int, max),
+                Ok(int) => (accum + int, max1, max2, max3),
                 Err(why) => panic!("Malformed input: {}", why)
             }}
             Err(why) => panic!("Malformed input: {}", why)
         }
     });
+    let finish = if res.0 > res.3 {
+        if res.0 > res.1 {
+             (res.0, res.0 + res.1 + res.2)
+        } else {(res.1, res.0 + res.1 + res.2)}
+    } else {(res.1, res.1 + res.2 + res.3)};
 
-    println!("{}", if res.0 > res.1 {res.0} else {res.1})
+    println!("{}", finish.0);
+    println!("{}", finish.1);
 }
