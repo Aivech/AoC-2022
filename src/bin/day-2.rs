c0d16e02ce0fac5fda6ae0e63e247e8fc54498bf
Aivech <16143392+Aivech@users.noreply.github.com>
2022-12-08 20:11:07 -0500

    Day 2 part 1

---
+++ src/bin/day-2.rs
@@ -0,0 +1,41 @@
+use std::env;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::path::Path;
+
+fn main() {
+    let args = env::args().collect::<Vec<String>>();
+
+    let path = match args.get(1) {
+        None => {
+            panic!("No input file found.");
+        }
+        Some(arg1) => Path::new(arg1)
+    };
+
+    let lines = match File::open(path) {
+        Ok(file) => BufReader::new(file).lines(),
+        Err(msg) => {panic!("Couldn't open \"{}\": {}", path.display(), msg)}
+    };
+
+    let score = lines.map(|line| -> (i32, i32) {
+        match line {
+            Ok(str) => if str.len() == 3 {
+                let mut chars = str.chars();
+                let them = chars.nth(0).unwrap_or('!') as i32 - 'A' as i32;
+                let me = chars.nth(2).unwrap_or('!') as i32 - 'X' as i32;
+                if 0 <= them && them <= 2 && 0 <= me && me <= 2 {
+                    return (them, me)
+                } else {panic!("Malformed input: \"{}\"", str)}
+            } else {panic!("Malformed input: \"{}\"", str)}
+            Err(why) => panic!("Malformed input: \"{}\"", why)
+        }
+    }).fold(0, |score, (them, me)| -> i32 {
+        let shape_score = me+1;
+        // i solved this puzzle by the power of MODULAR ARITHMETIC
+        let round_score = ((me-them).rem_euclid(3)+1).rem_euclid(3)*3;
+        score + shape_score + round_score
+    });
+
+    println!("Total score (part 1): {}", score);
+}
