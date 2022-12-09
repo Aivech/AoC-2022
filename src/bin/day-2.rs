aa43cd803120802f67cef905f8242cdda85c59c7
Aivech <16143392+Aivech@users.noreply.github.com>
2022-12-08 20:30:05 -0500

    Day 2 part 2

--- src/bin/day-2.rs
+++ src/bin/day-2.rs
@@ -18,7 +18,7 @@ fn main() {
         Err(msg) => {panic!("Couldn't open \"{}\": {}", path.display(), msg)}
     };
 
-    let score = lines.map(|line| -> (i32, i32) {
+    let (score1, score2) = lines.map(|line| -> (i32, i32) {
         match line {
             Ok(str) => if str.len() == 3 && str.is_ascii() {
                 let them = (str.as_bytes()[0] as i32) - ('A' as i32);
@@ -29,12 +29,18 @@ fn main() {
             } else {panic!("Malformed input: \"{}\"", str)}
             Err(why) => panic!("Malformed input: \"{}\"", why)
         }
-    }).fold(0, |score, (them, me)| -> i32 {
-        let shape_score = me+1;
+    }).fold((0,0), |(score1, score2), (them, me)| -> (i32,i32) {
+        let shape_score1 = me+1;
+        let round_score2 = me*3;
         // i solved this puzzle by the power of MODULAR ARITHMETIC
-        let round_score = ((me-them).rem_euclid(3)+1).rem_euclid(3)*3;
-        score + shape_score + round_score
+        // me - them + 1 = result (mod 3)
+        let round_score1 = (me-them +1).rem_euclid(3)*3;
+        // i solved the second puzzle by the power of MODULAR ARITHMETIC
+        // me - 1 + them = result (mod 3)
+        let shape_score2 = (me-1 + them).rem_euclid(3);
+        (score1 + shape_score1 + round_score1, score2 + shape_score2 + round_score2)
     });
 
-    println!("Total score (part 1): {}", score);
+    println!("Total score (part 1): {}", score1);
+    println!("Total score (part 2): {}", score2);
 }
