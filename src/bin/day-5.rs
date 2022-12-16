9089355a916bab53e4e75ccbe26d25016048c9d3
Aivech <16143392+Aivech@users.noreply.github.com>
2022-12-15 23:41:34 -0500

    removed old code

--- src/bin/day-5.rs
+++ src/bin/day-5.rs
@@ -166,68 +166,6 @@ fn main() {
             }
         });
 
-    // parse instructions (old method)
-    /* lines
-           .skip(cargo_depth as usize + 2)
-           .map(|line| -> String {
-               match line {
-                   Ok(str) => {
-                       if str.is_empty() || !str.is_ascii() {
-                           panic!("Malformed input: {}", str)
-                       };
-                       str
-                   }
-                   Err(why) => {
-                       panic!("Malformed input: {}", why)
-                   }
-               }
-           })
-           .map(|str| -> (u16, u16, u16) {
-               let result = str
-                   .chars()
-                   .filter(|c| c.is_ascii_digit() || c.is_whitespace())
-                   .collect::<String>()
-                   .split_whitespace()
-                   .flat_map(|s| s.parse::<u16>())
-                   .collect::<Vec<u16>>();
-               (result[0], result[1], result[2])
-           })
-           .for_each(|(move_count, source, target)| {
-               let source = source - 1;
-               let target = target - 1;
-               for _ in 0..move_count {
-                   let c = ship
-                       .remove(&(source, col_heights[source as usize] - 1))
-                       .unwrap_or_else(|| {
-                           panic!(
-                               "invalid move: {} from {} to {}",
-                               move_count,
-                               source + 1,
-                               target + 1
-                           )
-                       });
-                   ship.insert((target, col_heights[target as usize]), c);
-                   col_heights[source as usize] -= 1;
-                   col_heights[target as usize] += 1;
-               }
-           });
-
-    // print results
-    eprintln!("Final ship: -----");
-    for i in 0..ship_length {
-        print!("{}: ", i + 1);
-        for j in 0..col_heights[i as usize] {
-            print!("{} ", ship.get(&(i, j)).unwrap());
-        }
-        println!();
-    }
-    eprintln!("--------------");
-    for i in 0..ship_length {
-        print!("{}", ship.get(&(i, col_heights[i as usize] - 1)).unwrap());
-    }
-    println!();
-    */
-
     // print results: new method
     println!(
         "{}",
